c0ff7068d9450ecffecc8ee8c4e35e32913e92d2
AhmerZunair <30389585+AhmerZunair@users.noreply.github.com>
2023-09-01 11:12:58 +0100

    Add files via upload

--- Stream.py
+++ Stream.py
@@ -1,18 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.write("""
 # My first app
 Hello *world!*
