e11797a0d2a5d69121a289ef87dab137416bb7f3
AhmerZunair <30389585+AhmerZunair@users.noreply.github.com>
2023-09-01 10:38:25 +0100

    Add files via upload

---
+++ Stream.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.read_excel("C:/Users/user8/Desktop/RM_Report_RM_Data 2.xlsx")
+#df.head()
+#st.line_chart(df, x=df['RM_Release_Status'], y=df['Final_Scope_For_Migration'])
+
+
+chart_data = pd.DataFrame({
+    'col1' : np.random.randn(20),
+    'col2' : np.random.randn(20),
+    'col3' : np.random.choice(['A','B','C'], 20)
+})
+
+st.line_chart(
+    chart_data,
+    x = 'col1',
+    y = 'col2',
+    color = 'col3'
+)
